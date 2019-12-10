e2ff610105a7c8211f138fb7a10e0723ae602c4f
Sanjeevani Bhavsar <sanjibhavsar@wustl.edu>
2019-12-09 19:40:18 -0600

    excel output

--- tools/queryDB.R
+++ tools/queryDB.R
@@ -5,6 +5,7 @@ suppressMessages(library(dplyr))
 suppressMessages(library(optparse))
 suppressMessages(library(stringr))
 suppressMessages(library(jsonlite))
+suppressMessages(library(xlsx))
 
 # read in options from command line code
 parse_arguments <- function() {
@@ -40,38 +41,38 @@ parse_arguments <- function() {
 create_db <- function(fqFiles, libFiles, s2Files, s1Files, rnaFiles, bioFiles, output){
   
   tblFastQ <- sapply(fqFiles, read_excel, simplify=FALSE)  %>%
-    bind_rows(.id = "id") %>% 
-    select(-id)
+      bind_rows(.id = "id") %>% 
+      select(-id)
   # drops all observations which do not have a corresponding FastQ filepath.
-  tblFastQ <- tblFastQ %>% drop_na(fastqFileName)
+    tblFastQ <- tblFastQ %>% drop_na(fastqFileName)
   tblLib <- sapply(libFiles, read_excel, simplify=FALSE) %>%
-    bind_rows(.id = "id") %>% 
-    select(-id) 
+      bind_rows(.id = "id") %>% 
+      select(-id) 
   tblS2 <- sapply(s2Files, read_excel, simplify=FALSE) %>%
-    bind_rows(.id = "id") %>% 
-    select(-id) 
+      bind_rows(.id = "id") %>% 
+      select(-id) 
   tblS1 <- sapply(s1Files, read_excel, simplify=FALSE) %>%
-    bind_rows(.id = "id") %>% 
-    select(-id) 
+      bind_rows(.id = "id") %>% 
+      select(-id) 
   tblRNA <- sapply(rnaFiles, read_excel, simplify=FALSE) %>%
-    bind_rows(.id = "id") %>% 
-    select(-id) 
+      bind_rows(.id = "id") %>% 
+      select(-id) 
   tblBio <- sapply(bioFiles, read_excel, simplify=FALSE) %>%
-    bind_rows(.id = "id") %>% 
-    select(-id)
+      bind_rows(.id = "id") %>% 
+      select(-id)
   
   # perform all joins
   db <- tblFastQ %>%
-    left_join(tblLib) %>%
-    left_join(tblS2) %>%
-    left_join(tblS1) %>%
-    left_join(tblRNA) %>%
-    left_join(tblBio)
+        left_join(tblLib) %>%
+        left_join(tblS2) %>%
+        left_join(tblS1) %>%
+        left_join(tblRNA) %>%
+        left_join(tblBio)
   
   # adds vectors holding path to fastq and tsv files
   db <- db %>% mutate(fastqFilePath = paste("sequence/run_", runNumber, "_samples", fastqFileName, sep = ""))
   db <- db %>% mutate(tsvFilePath = gsub(".fastq.gz", "_read_count.tsv", fastqFilePath))
-  
+ 
   # prints database to csv file - code validation
   # write_excel_csv(db, paste(output, "database.csv"))
   return(db)
@@ -85,22 +86,24 @@ query_db <- function(df, cols, conds){
 }
 
 write_summary <- function(query, qname, output){
-  
+
   selectQ <- query %>% select(genotype, strain, inductionDelay, libraryDate, harvestDate, replicate, runNumber, index1Sequence, index2Sequence, fastqFileName, timePoint, floodmedia)
   # print(selectQ)
   summary <- mutate(selectQ,  ST_PIPE = "",
-                    ST_TOTAL_READS	= "",
-                    ST_ALIGN_PCT	= "",
-                    ST_MUT_FOW	= "",
-                    ST_RC_FOM	= "",
-                    ST_COV_MED	= "",
-                    AUTO_AUDIT	= "",
-                    MANUAL_AUDIT	= "",
-                    USER	= "",
-                    NOTE = "")
+                   ST_TOTAL_READS	= "",
+                   ST_ALIGN_PCT	= "",
+                   ST_MUT_FOW	= "",
+                   ST_RC_FOM	= "",
+                   ST_COV_MED	= "",
+                   AUTO_AUDIT	= "",
+                   MANUAL_AUDIT	= "",
+                   USER	= "",
+                   NOTE = "")
   
   # write file to csv at specified filepath
-  write_excel_csv(summary, paste(output, qname, "sample_summary.csv", sep = "/"))
+  # write_excel_csv(summary, paste(output, qname, "sample_summary.csv", sep = "/"))
+  write.xlsx(x = as.data.frame(summary), file = paste(output, qname, "sample_summary.xlsx", sep = "/"), row.names = FALSE)
+  
   return()
 }
 
@@ -143,7 +146,8 @@ df <- unnest(json_df, cols = everything())
 query <- query_db(db, cols = as.list(names(df)), conds = as.list(df))
 
 # write output files
-write_excel_csv(query, paste(parsed$output, parsed$query, "queriedDB.csv", sep = "/"))
+# write_excel_csv(query, paste(parsed$output, parsed$query, "queriedDB.csv", sep = "/"))
+write.xlsx(x = as.data.frame(query), file = paste(parsed$output, parsed$query, "queriedDB.xlsx", sep = "/"), row.names = FALSE)
 write_summary(query, qname = parsed$query, output = parsed$output)
 write_lookup(query, qname = parsed$query, output = parsed$output)
 
